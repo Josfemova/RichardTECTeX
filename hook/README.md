6ee2c02332df660448af379c505cd4a33a0c5010
Ignacio Vargas <ignaevc@gmail.com>
2021-05-15 13:12:29 -0600

    Added README.md and fixed the install script so that it always runs from the same directory and gives permissions to the hooks

---
+++ hook/README.md
@@ -0,0 +1,14 @@
+# Instalación de hooks
+**Nota:** Todos los snippets mostrados a continuación se corren desde el root folder del proyecto.
+
+Para instalar los git hooks primero debe darle permiso al script instalador:
+```
+chmod +x hook/pre-commit-hook-install.sh
+```
+
+Luego corre el script:
+```
+hook/pre-commit-hook-install.sh
+```
+
+y ya!
--- hook/pre-commit-hook-install.sh
+++ hook/pre-commit-hook-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd $(dirname "$0") # set directory to hook/
+
 #create the file if it doesn't exist
 touch ../.git/hooks/pre-commit 
 #delete the file
@@ -8,3 +10,10 @@ rm ../.git/hooks/pre-commit
 ln -s pre-commit-hook.sh ../.git/hooks/pre-commit 
 #copy paste the code in the hook folder
 cp pre-commit-hook.sh ../.git/hooks/pre-commit-hook.sh
+
+#give permissions to scripts
+chmod +x ../.git/hooks/pre-commit
+chmod +x ../.git/hooks/pre-commit-hook.sh
+
+echo "hook instalado correctamente"
+
